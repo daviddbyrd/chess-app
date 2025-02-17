4509a9478878284b5fb199d06c205641afa28270
daviddbyrd <davbywork@gmail.com>
2025-02-17 17:29:57 +0000

    added pieces, still need to fix things

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import { useState } from "react";
 import Square from "./Square.jsx";
 
 const Board = () => {
+  const [pieces, setPieces] = useState([
+    ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
+    ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+    Array(8).fill(null),
+    Array(8).fill(null),
+    Array(8).fill(null),
+    Array(8).fill(null),
+    ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+    ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"],
+  ]);
+
   const rows = 8;
   const cols = 8;
 
   return (
     <div className="grid grid-cols-8">
-      {Array.from({ length: rows }).map((_, rowIndex) =>
-        Array.from({ length: cols }).map((_, colIndex) => (
-          <Square row={rowIndex} col={colIndex} />
+      {pieces.map((row, rowIndex) =>
+        row.map((piece, colIndex) => (
+          <Square row={rowIndex} col={colIndex} piece={piece} />
         ))
       )}
     </div>
--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import bbi from "../assets/bb.png";
+import bki from "bk.png";
+import bni from "bn.png";
+import bpi from "bp.png";
+import bqi from "bq.png";
+import bri from "br.png";
+import wbi from "wb.png";
+import wki from "wk.png";
+import wni from "wn.png";
+import wpi from "wp.png";
+import wqi from "wq.png";
+import wri from "wr.png";
 
-const Square = ({ row, col }) => {
-  const darkbrown = "#A97D54";
-  const lightbrown = "#D3B895";
 
+const Square = ({ row, col, piece }) => {
   return (
     <div
       className={`w-20 h-20 ${
         (row + col) % 2 === 0 ? "bg-green-300" : "bg-stone-300"
       }`}
     >
+      {piece && <img className="w-20 h-20" src={`${piece}i`} />}
       {row}
       {String.fromCharCode(col + 97)}
     </div>

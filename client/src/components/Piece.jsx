b17ddb2b42525ccbcdc077292ac23a66cabbef18
daviddbyrd <davbywork@gmail.com>
2025-03-06 23:20:07 +0000

    added drag functionality, bug with piece remaining after drag

--- client/src/components/Piece.jsx
+++ client/src/components/Piece.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 const Piece = ({ row, col, piece, handleClick }) => {
+  const handleDragStart = (e) => {
+    e.dataTransfer.setData("text/plain", `${piece}${row}${col}`);
+    console.log(e);
+  };
+
   return (
     <div
       className="w-20 h-20 bg-cover bg-center"
       style={{ backgroundImage: `url('../../public/${piece}.png')` }}
       onClick={() => handleClick(row, col, piece)}
+      draggable="true"
+      onDragStart={handleDragStart}
     ></div>
   );
 };
--- client/src/components/Pieces.jsx
+++ client/src/components/Pieces.jsx
@@ -15,37 +15,23 @@ const Pieces = () => {
   ]);
   const [currentPiece, setCurrentPiece] = useState(null);
   const [currentTurn, setCurrentTurn] = useState("w");
-  const [kingPos, setKingPos] = useState({
-    w: { row: 0, col: 4 },
-    b: { row: 7, col: 4 },
-  });
 
-  const movePiece = (newRow, newCol) => {
-    if (
-      isValidMove(
-        pieces,
-        currentPiece.row,
-        currentPiece.col,
-        newRow,
-        newCol,
-        currentTurn
-      )
-    ) {
+  const movePiece = (oldRow, oldCol, newRow, newCol) => {
+    if (isValidMove(pieces, oldRow, oldCol, newRow, newCol, currentTurn)) {
+      const newPiece = pieces[oldRow][oldCol];
       const nextPieces = pieces.map((row, rowIndex) =>
         row.map((piece, colIndex) => {
-          if (rowIndex === currentPiece.row && colIndex === currentPiece.col) {
+          if (rowIndex === oldRow && colIndex === oldCol) {
             return null;
           } else if (rowIndex === newRow && colIndex === newCol) {
-            return currentPiece.piece;
+            return newPiece;
           } else {
             return piece;
           }
         })
       );
       setPieces(nextPieces);
-      if (currentPiece.piece[1] === "k") {
-        setKingPos({ ...kingPos, currentTurn: { row: newRow, col: newCol } });
-      }
+      console.log(nextPieces);
       if (currentTurn === "b") {
         setCurrentTurn("w");
       } else {
@@ -57,7 +43,7 @@ const Pieces = () => {
 
   const handleClick = (row, col, piece) => {
     if (currentPiece) {
-      movePiece(row, col);
+      movePiece(currentPiece.row, currentPiece.col, row, col);
     } else {
       if (piece) {
         setCurrentPiece({ row, col, piece });
@@ -65,8 +51,33 @@ const Pieces = () => {
     }
   };
 
+  const handleDrop = (e) => {
+    let board = e.currentTarget.getBoundingClientRect();
+    let rowDist = e.clientY - board.top;
+    let colDist = e.clientX - board.left;
+
+    let rowHeight = board.height / 8;
+    let colWidth = board.width / 8;
+
+    let newRow = 7 - Math.floor(rowDist / rowHeight);
+    let newCol = Math.floor(colDist / colWidth);
+
+    let data = e.dataTransfer.getData("text/plain");
+    console.log(data);
+    let row = data[2];
+    let col = data[3];
+
+    movePiece(row, col, newRow, newCol);
+  };
+
+  const handleDragOver = (e) => e.preventDefault();
+
   return (
-    <div className="grid grid-cols-8 absolute">
+    <div
+      className="grid grid-cols-8 absolute"
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+    >
       {pieces.toReversed().map((row, reversedIndex) => {
         const rowIndex = 7 - reversedIndex;
         return row.map((piece, colIndex) => (

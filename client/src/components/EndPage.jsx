836a54a5ea6b5f585d107e0989e5b75af88d1865
daviddbyrd <davbywork@gmail.com>
2025-03-21 21:33:56 +0000

    added stlying to end page

--- client/src/components/EndPage.jsx
+++ client/src/components/EndPage.jsx
@@ -5,14 +5,16 @@ const EndPage = ({ state, turn, rematch }) => {
   console.log(turn);
   const convert = { w: "Black", b: "White" };
   return (
-    <div className="absolute inset-0 m-auto w-32 h-32 bg-white flex flex-col items-center justify-center rounded-lg border-4 border-black">
+    <div className="absolute inset-0 m-auto w-60 h-80 bg-white flex flex-col justify-center items-center rounded-xl shadow-lg">
       {state === "checkmate" ? (
-        <div className="text-center">{convert[turn]} wins by Checkmate</div>
+        <div className="text-center mt-20 text-4xl text-gray-900">
+          {convert[turn]} wins by Checkmate
+        </div>
       ) : (
-        <div className="text-center">Stalemate!</div>
+        <div className="text-center">Match Ended in Stalemate</div>
       )}
       <button
-        className="rounded-md border-1 border-black cursor-pointer"
+        className="rounded-md cursor-pointer mt-10 mb-10 w-40 h-20 shadow-lg bg-gradient-to-br from-blue-100 via-purple-100 to-pink-100 backdrop-blur text-2xl"
         onClick={rematch}
       >
         Rematch

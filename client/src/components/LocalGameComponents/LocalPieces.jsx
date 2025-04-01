f3893ccd29ad02b7ea1cd0f9a9d24d880c850734
daviddbyrd <davbywork@gmail.com>
2025-04-02 00:28:04 +0100

    updated LocalGame to be the same as ComputerGame, except for computer game specific features

--- client/src/components/LocalGameComponents/LocalPieces.jsx
+++ client/src/components/LocalGameComponents/LocalPieces.jsx
@@ -13,6 +13,7 @@ import {
   isStalemate,
   findTakenPiece,
   convertCastlingAvailability,
+  isEnPassantTarget,
 } from "../../utils/Helper.js";
 import { defaultPieces } from "../../utils/DefaultPieces.js";
 import Board from "../Board.jsx";
@@ -20,6 +21,7 @@ import MoveSoundEffect from "../../assets/move.mp3";
 import CaptureSoundEffect from "../../assets/capture.mp3";
 
 const LocalPieces = () => {
+  const playerColour = "w";
   const [pieces, setPieces] = useState(defaultPieces);
   const boardRef = useRef(null);
   const [currentPiece, setCurrentPiece] = useState(null);
@@ -30,7 +32,7 @@ const LocalPieces = () => {
     "k",
     "q",
   ]);
-  const [prevMove, setPrevMove] = useState(null);
+  const [enPassantAvailability, setEnPassantAvailabillity] = useState(null);
   const [promoting, setPromoting] = useState(false);
   const [gamestate, setGamestate] = useState(null);
   const [takenPieces, setTakenPieces] = useState({
@@ -53,21 +55,34 @@ const LocalPieces = () => {
         newCol,
         currentTurn,
         castlingAvailability,
-        prevMove
+        enPassantAvailability
       )
     ) {
+      // if (pieces[oldRow][oldCol][0] !== playerColour) {
+      //   setCurrentPiece(null);
+      //   return;
+      // }
+
       if ("kr".includes(pieces[oldRow][oldCol][1])) {
         let changedIndices = convertCastlingAvailability(
           pieces[oldRow][oldCol],
           oldCol
         );
+
         setCastlingAvailability((prev) => {
           changedIndices.forEach((i) => {
-            prev[i] = "-";
+            prev[i] = "";
           });
           return prev;
         });
       }
+
+      if (isEnPassantTarget(pieces, oldRow, oldCol, newRow, newCol)) {
+        setEnPassantAvailabillity(`${newCol}${newRow}`);
+      } else {
+        setEnPassantAvailabillity(null);
+      }
+
       const takenPiece = findTakenPiece(
         pieces,
         oldRow,
@@ -101,7 +116,6 @@ const LocalPieces = () => {
       }
       const nextPieces = constructBoard(pieces, oldRow, oldCol, newRow, newCol);
       setPieces(nextPieces);
-      setPrevMove(`${oldRow}${oldCol}${newRow}${newCol}`);
       if (isPromoting(nextPieces)) {
         setPromoting({ row: newRow, col: newCol });
       } else {
@@ -155,7 +169,7 @@ const LocalPieces = () => {
 
   const handleDragOver = (e) => e.preventDefault();
 
-  const handlePromotionClick = (row, col, piece) => {
+  const handlePromotionClick = async (row, col, piece) => {
     const nextPieces = constructBoardPromotion(pieces, row, col, piece);
     setPieces(nextPieces);
     setPromoting(false);
